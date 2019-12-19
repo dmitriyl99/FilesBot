9f79c72fa97cba913c39955d58deada6deea791e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-19 13:49:43 +0500

    CRUD views for categories

--- filebot/views/catalog.py
+++ filebot/views/catalog.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import SingleObjectMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 from filebot.models import Category
+from filebot.forms import CategoryForm
+from django.urls import reverse_lazy
 
 
 class CategoryListView(LoginRequiredMixin, ListView):
@@ -43,3 +47,40 @@ class ShowCategoryFilesView(LoginRequiredMixin, ListView, SingleObjectMixin):
 
     def get_queryset(self):
         return self.object.file_set.all()
+
+
+class CreateCategoryView(LoginRequiredMixin, CreateView):
+    model = Category
+    fields = ['name', 'parent']
+    template_name = 'admin/catalog/categories/create.html'
+    form_class = CategoryForm
+    context_object_name = 'form'
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, "Категория %s добавлена!" % form.cleaned_data['name'])
+        return result
+
+
+class UpdateCategoryView(LoginRequiredMixin, UpdateView):
+    model = Category
+    fields = ['name', 'parent']
+    template_name = 'admin/catalog/categories/edit.html'
+    form_class = CategoryForm
+    context_object_name = 'form'
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, "Категория %s изменена!" % form.cleaned_data['name'])
+        return result
+
+
+class DeleteCategoryView(LoginRequiredMixin, DeleteView):
+    model = Category
+    success_url = reverse_lazy('admin_categories')
+
+    def delete(self, request, *args, **kwargs):
+        category_name = self.get_object().name
+        result = super().delete(request, *args, **kwargs)
+        messages.success(self.request, "Категория %s удалена!" % category_name)
+        return result
