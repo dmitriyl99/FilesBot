5a3ac4f7553c7e0988892adb393816ececbfabfb
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-20 12:57:49 +0500

    Change url names

--- filebot/urls.py
+++ filebot/urls.py
@@ -3,11 +3,11 @@ from .views import index, catalog, settings, bot
 from FileTelegramBot.settings import WEBHOOK_URL_PATH
 
 urlpatterns = [
-    path('', index.IndexView.as_view(), name='admin_home'),
-    path('catalog/', catalog.CategoryListView.as_view(), 'admin_catalog'),
-    path('settings/', settings.SettingsView.as_view(), 'admin_settings'),
-    path('catalog/<int:pk>', catalog.ShowCategoryChildrenView.as_view(), 'admin_catalog_categories_children'),
-    path('catalog/<int:pk>/files', catalog.ShowCategoryFilesView.as_view(), 'admin_catalog_categories_files'),
+    path('', index.IndexView.as_view(), name='admin-home'),
+    path('catalog/', catalog.CategoryListView.as_view(), name='admin-catalog'),
+    path('settings/', settings.SettingsView.as_view(), name='admin-settings'),
+    path('catalog/<int:pk>', catalog.ShowCategoryChildrenView.as_view(), name='admin-catalog-categories-children'),
+    path('catalog/<int:pk>/files', catalog.ShowCategoryFilesView.as_view(), name='admin-catalog-categories-files'),
 
     path('init/', bot.BotInitializeView.as_view()),
     path(WEBHOOK_URL_PATH, bot.BotUpdatesRecieverView.as_view())
