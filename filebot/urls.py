a6e33b6eef77ab82f9d24b1317ccdc0448f46057
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-20 13:00:25 +0500

    Rename catalog view to category

--- filebot/urls.py
+++ filebot/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import index, catalog, settings, bot
+from .views import index, categories, settings, bot
 from FileTelegramBot.settings import WEBHOOK_URL_PATH
 
 urlpatterns = [
     path('', index.IndexView.as_view(), name='admin-home'),
-    path('catalog/', catalog.CategoryListView.as_view(), name='admin-catalog'),
+    path('catalog/', categories.CategoryListView.as_view(), name='admin-catalog'),
     path('settings/', settings.SettingsView.as_view(), name='admin-settings'),
-    path('catalog/<int:pk>', catalog.ShowCategoryChildrenView.as_view(), name='admin-catalog-categories-children'),
-    path('catalog/<int:pk>/files', catalog.ShowCategoryFilesView.as_view(), name='admin-catalog-categories-files'),
+    path('catalog/<int:pk>', categories.ShowCategoryChildrenView.as_view(), name='admin-catalog-categories-children'),
+    path('catalog/<int:pk>/files', categories.ShowCategoryFilesView.as_view(), name='admin-catalog-categories-files'),
 
     path('init/', bot.BotInitializeView.as_view()),
     path(WEBHOOK_URL_PATH, bot.BotUpdatesRecieverView.as_view())
