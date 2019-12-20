cec8c14c179716c286be5570153dfe718b7f7fe9
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-20 14:58:57 +0500

    Add CRUD urls for category

--- filebot/urls.py
+++ filebot/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('settings/', settings.SettingsView.as_view(), name='admin-settings'),
     path('catalog/<int:pk>', categories.ShowCategoryChildrenView.as_view(), name='admin-catalog-categories-children'),
     path('catalog/<int:pk>/files', categories.ShowCategoryFilesView.as_view(), name='admin-catalog-categories-files'),
+    path('catalog/category/add', categories.CreateCategoryView.as_view(), name='admin-catalog-category-add'),
+    path('catalog/category/<int:pk>/edit', categories.UpdateCategoryView.as_view(), name='admin-catalog-category-edit'),
+    path('catalog/category/<int:pk>/remove', categories.DeleteCategoryView.as_view(), name='admin-catalog-category-remove'),
 
     path('init/', bot.BotInitializeView.as_view()),
     path(WEBHOOK_URL_PATH, bot.BotUpdatesRecieverView.as_view())
