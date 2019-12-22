73cf45123ddc30659ebdbb71e6d5c12b4759df29
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-22 20:33:48 +0500

    Settings update view

--- filebot/views/settings.py
+++ filebot/views/settings.py
@@ -1,11 +1,20 @@
-from django.views.generic import DetailView
+from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from filebot.models import Settings
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 
-class SettingsView(LoginRequiredMixin, DetailView):
+class SettingsView(LoginRequiredMixin, UpdateView):
     template_name = 'admin/settings/index.html'
-    context_object_name = 'settings'
+    model = Settings
+    fields = ['share_text', 'contacts_text', 'start_message_text']
+    success_url = reverse_lazy('admin-settings')
 
     def get_object(self, queryset=None):
         return Settings.objects.first()
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        messages.success(self.request, 'Настройки сохранены!')
+        return result
