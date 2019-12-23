3b3698b0adf989c330c529a4fa5d0afe1e24737e
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-23 18:10:08 +0500

    Send photo in share part

--- bot/share.py
+++ bot/share.py
@@ -11,6 +11,10 @@ def share_handler(message: Message):
 
     bot_settings = settings.get_settings()
     if bot_settings and bot_settings.share_text:
-        telegram_bot.send_message(chat_id, bot_settings.share_text, parse_mode='HTML')
+        if bot_settings.share_photo:
+            telegram_bot.send_photo(chat_id, open(bot_settings.share_photo.path, 'rb'),
+                                    caption=bot_settings.share_text, parse_mode='HTML')
+        else:
+            telegram_bot.send_message(chat_id, bot_settings.share_text, parse_mode='HTML')
     else:
         telegram_bot.send_message(chat_id, strings.get_string('share.no_share_text'))
