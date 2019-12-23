b14b1f3a0b567c9e487fa1a99a2363e46d896fee
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-23 18:08:10 +0500

    Serve media files

--- FileTelegramBot/urls.py
+++ FileTelegramBot/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import include, url
 
 urlpatterns = [
     path(r'accounts/', include('django.contrib.auth.urls')),
     path('', include('filebot.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
    import debug_toolbar
