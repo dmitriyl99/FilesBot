200f8ff56639622549f9fab790458d0484802fec
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-27 14:05:07 +0500

    Fix return value in advertising post view

--- filebot/views/settings.py
+++ filebot/views/settings.py
@@ -45,3 +45,5 @@ class AdvertisingPostView(LoginRequiredMixin, FormView):
         if file:
             file_storage.delete(filename)
         messages.success(self.request, "Рассылка прошла успешно!")
+        result = super().form_valid(form)
+        return result
