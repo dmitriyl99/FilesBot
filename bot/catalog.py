c530e22453bb2d58383760b328fcb5817224b803
dmitriyl99 <dmitriyl1899@gmail.com>
2019-12-28 14:15:01 +0500

    Fix go back from files from users

--- bot/catalog.py
+++ bot/catalog.py
@@ -5,7 +5,10 @@ from core import files, users
 from resources import strings, keyboards
 
 
-def _go_back(user_id, current_category):
+def _go_back(user_id, current_category, from_users=False):
+    if from_users:
+        Navigation.to_catalog(user_id)
+        return
     if not current_category:
         Navigation.to_main_menu(user_id)
         return
@@ -52,7 +55,8 @@ def category_handler(message: Message, *args, **kwargs):
             select_message = strings.get_string('catalog.files.select')
             files_keyboard = keyboards.from_files_list_to_keyboard(user_files)
             telegram_bot.send_message(user_id, select_message, reply_markup=files_keyboard)
-            telegram_bot.register_next_step_handler_by_chat_id(user_id, file_handler, current_category=None)
+            telegram_bot.register_next_step_handler_by_chat_id(user_id, file_handler, current_category=None,
+                                                               from_users=True)
             return
         category = files.get_category_by_name(message.text, current_category)
         if not category:
@@ -89,7 +93,10 @@ def file_handler(message: Message, *args, **kwargs):
         error()
         return
     if strings.get_string('back') in message.text:
-        _go_back(user_id, current_category)
+        if 'from_users' in kwargs:
+            _go_back(user_id, current_category, from_users=True)
+        else:
+            _go_back(user_id, current_category)
     else:
         file = files.get_file_by_name(message.text)
         if not file:
